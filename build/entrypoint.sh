79f61f2e02497753f9bd276b12a03f45d887e7e8
Dziubin Anton <a.dziubin@vr-dev.team>
2019-04-03 17:42:16 +0300

    initila commit

---
+++ build/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Update suricata rules
+suricata-update
+
+# Start cron
+crond
+
+# Add cronjob
+crontab /etc/cron.d/suricata-update-cron
+
+# Started suricata
+/usr/bin/suricata -c /etc/suricata/suricata.yaml -q 0
